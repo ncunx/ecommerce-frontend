35bb4095abcb51840a9e1f4aaad9bd0b2928fbda
Kamil Kosiba <kamil.kosiba@outlook.com>
2019-03-27 20:23:50 +0000

    Added authResetPassword action

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -6,6 +6,7 @@ export const AUTH_SUCCESS = "AUTH_SUCCESS";
 export const AUTH_FAIL = "AUTH_FAIL";
 export const AUTH_LOGOUT = "AUTH_LOGOUT";
 export const AUTH_CLEAR_ERRORS = "AUTH_CLEAR_ERRORS";
+export const AUTH_RESET_PASSWORD = "AUTH_RESET_PASSWORD";
 
 
 export const authStart = () => {
@@ -41,6 +42,12 @@ export const authLogout = () => {
 export const authClearErrors = () => {
   return {
     type: AUTH_CLEAR_ERRORS
+  };
+};
+
+export const authResetPassword = () => {
+  return {
+    type: AUTH_RESET_PASSWORD
   };
 };
 
